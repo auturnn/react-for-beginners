eef65a5140d745fc1b3c065cdd9c2d2e54bd5ff4
auturnn <acki@kakao.com>
2022-05-12 04:01:33 +0900

    learning to useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import Button from "./Button";
+import React, { useState, useEffect } from "react";
+// import Button from "./Button";
 
 function App() {
+  const [counter, setValue]: any = useState(0);
+  const [keyword, setKeyword]: any = useState("");
+  const onChange = (event: any) => {
+    setKeyword(event.target.value);
+  };
+  const onClick = () => setValue((prev: number) => prev + 1);
+
+  //어떤일이 일어나도 아래의 코드는 딱한번만 실행
+  useEffect(() => {
+    console.log("running only one time");
+  }, []);
+  //keyword가 변할때만 실행할 것이다.
+  //useEffect = 변할때 실행하는 함수, trigger가 되는 대상(dependencyList)
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log(keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1>Start React!!</h1>
-      <Button text="hi!" fontSize={20} />
+      <input value={keyword} type="text" onChange={onChange} />
+      {/* <h1>Start React!!</h1>
+      <Button text="hi!" fontSize={20} /> */}
+      {counter}
+      <button onClick={onClick}>plus</button>
     </div>
   );
 }
