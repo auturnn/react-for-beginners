0e602d875a952b3d7009a8d01beae7e29cc9ca69
auturnn <acki@kakao.com>
2022-05-12 05:50:40 +0900

    created Todo List

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import React, { useState, useEffect } from "react";
-
-function Hello() {
-  function byeFn() {
-    console.log("bye");
-  }
-
-  function hiFn() {
-    console.log("hi");
-    return byeFn; //cleanUp (컴포넌트를 소멸시킬때 실행될 함수)
-  }
-
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing]: any = useState(false);
-  const onClick = () => {
-    setShowing((prev: boolean) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDoList, setToDoList] = useState([""]);
+  const onChange = (event: any) => setTodo(event.target.value);
+  const onDeleted = (todoIndex: number) => {
+    setToDoList((currentArr: string[]) =>
+      currentArr.filter((_, idx) => idx !== todoIndex)
+    );
+  };
+  const onAdded = (event: any) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDoList((prevArr: string[]) => [toDo, ...prevArr]); //state는 언제나 새로 생성
+    setTodo(""); //empty value
   };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My Simple ToDo App</h1>
+      <ul>
+        {toDoList.map((item, idx) => {
+          if (item === "") {
+            return null;
+          }
+          return (
+            <li key={idx}>
+              <span>{item}</span>
+              <button
+                style={{
+                  backgroundColor: "initial",
+                  borderColor: "black",
+                  borderRadius: "6px",
+                  marginLeft: "10px",
+                  color: "red",
+                }}
+                onClick={() => onDeleted(idx)}
+              >
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+      <input
+        type="text"
+        value={toDo}
+        onChange={onChange}
+        placeholder="write your to do"
+      />
+      <button onClick={onAdded}>add</button>
     </div>
   );
 }
