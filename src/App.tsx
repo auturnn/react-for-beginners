76633479538a63f9e18ce9dc556a2e1241f0666f
auturnn <acki@kakao.com>
2022-05-12 04:13:17 +0900

    learning to cleanUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
-// import Button from "./Button";
+
+function Hello() {
+  function byeFn() {
+    console.log("bye");
+  }
+
+  function hiFn() {
+    console.log("hi");
+    return byeFn; //cleanUp (컴포넌트를 소멸시킬때 실행될 함수)
+  }
+
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setValue]: any = useState(0);
-  const [keyword, setKeyword]: any = useState("");
-  const onChange = (event: any) => {
-    setKeyword(event.target.value);
+  const [showing, setShowing]: any = useState(false);
+  const onClick = () => {
+    setShowing((prev: boolean) => !prev);
   };
-  const onClick = () => setValue((prev: number) => prev + 1);
-
-  //어떤일이 일어나도 아래의 코드는 딱한번만 실행
-  useEffect(() => {
-    console.log("running only one time");
-  }, []);
-  //keyword가 변할때만 실행할 것이다.
-  //useEffect = 변할때 실행하는 함수, trigger가 되는 대상(dependencyList)
-  useEffect(() => {
-    if (keyword !== "") {
-      console.log(keyword);
-    }
-  }, [keyword]);
   return (
     <div>
-      <input value={keyword} type="text" onChange={onChange} />
-      {/* <h1>Start React!!</h1>
-      <Button text="hi!" fontSize={20} /> */}
-      {counter}
-      <button onClick={onClick}>plus</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
