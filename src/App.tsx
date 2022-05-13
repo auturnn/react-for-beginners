20618072b99742c103268dd58cd92081493d8eae
auturnn <acki@kakao.com>
2022-05-14 05:35:49 +0900

    clear coinTranslater

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,63 @@
-import { useEffect, useState, ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState} from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [coinPrice, setCoinPrice] = useState(0);
-  const [coins, setCoins] = useState([
-    {
-      id: "",
-      name: "",
-      rank: 0,
-      symbol: "",
-      quotes: {
-        USD: {
-          price: 0,
-        },
-      },
-    },
-  ]);
-  const [coinName, setCoinName] = useState("");
-  const [inputMoney, setInputMoney] = useState(0);
-
-  const onChangeFn = (e: ChangeEvent<HTMLInputElement>) => {
-    if (typeof e.target.value !== "number") {
-      return;
+  const [inputData, setInputData] = useState(0);
+  const [coins, setCoins] = useState([{
+    id: "",
+    name: "",
+    symbol: "",
+    quotes: {
+      USD : {
+        price: 0
+      }
     }
+  }]);
 
-    setInputMoney(e.target.value);
-  };
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (typeof +e.target.value !== "number"){
+      return ;
+    }
+    setInputData(+e.target.value);
+  }
 
-  const onSelectFn = (e: ChangeEvent<HTMLSelectElement>) => {
-    setCoinName(e.target.value);
-    coins.filter((coin) => coin.name === e.target.value);
-    // setCoinPrice();
-  };
+  const onSelect = (e: ChangeEvent<HTMLSelectElement>) =>{
+    if (typeof +e.target.value !== "number"){
+      return ;
+    }
+    setCoinPrice(+e.target.value);
+  }
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((res) => res.json())
       .then((json) => {
-        setCoins(json);
         setLoading(false);
+        setCoins(json);
       });
   }, []);
 
-  function getPage() {
+  function getCoins() {
     return (
-      <div key="inputMoney">
-        <span>$ : </span>
-        <input
-          type="number"
-          style={{
-            width: "500px",
-          }}
-          onChange={onChangeFn}
-        />
-        <hr />
-        <select
-          style={{
-            width: "100px",
-          }}
-          onChange={onSelectFn}
-        >
-          {coins.map((coin) => {
-            return (
-              <option key={coin.id} value={coin.name}>
-                {coin.name} ({coin.symbol})
-              </option>
-            );
-          })}
+      <div>
+        ( $ ) : <input type="number" onChange={onInputChange} placeholder="Input Dollor" />
+        <hr/>
+        <select onChange={onSelect} style={{width:"187px"}}>
+          <option value="0" defaultChecked>Select Coin</option>
+        {coins.map((coin)=> {
+          return (<option value={coin.quotes.USD.price} key={coin.id}>{coin.name} ({coin.symbol})</option>)
+        })}
         </select>
-        {/* <input type="number" value={} /> */}
+        <input type="number" value={coinPrice*inputData} disabled/>
       </div>
     );
   }
+
   return (
-    <div key={"hi"}>
-      <h1>The Coins {loading ? null : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : getPage()}
+    <div key={"coinSelect"}>
+      <h1>The Coins {loading ? null : `(${coins.length}`})</h1>
+      {loading ? <strong>Loading...</strong> : getCoins()}
     </div>
   );
 }
